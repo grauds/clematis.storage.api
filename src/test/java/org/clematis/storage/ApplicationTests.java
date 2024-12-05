4951807407d157c207aac63667749b5fd960baa5
Anton Troshin <11165918+grauds@users.noreply.github.com>
2024-12-05 10:39:08 +0300

    [+] added test profile

--- src/test/java/org/clematis/storage/ApplicationTests.java
+++ src/test/java/org/clematis/storage/ApplicationTests.java
@@ -2,8 +2,10 @@ package org.clematis.storage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class ApplicationTests {
 
     @Test
