0d08b1ae81da3c9af641fa66b71700323bc9d140
Anton Troshin <11165918+grauds@users.noreply.github.com>
2024-12-12 18:47:08 +0300

    [+] added web cors

---
+++ src/main/java/org/clematis/storage/config/WebConfig.java
@@ -0,0 +1,19 @@
+package org.clematis.storage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    public static final String ALL_REGEXP = "/**";
+    public static final String ORIGINS = "*";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping(ALL_REGEXP);
+    }
+}
